938c35b620af694723cceed34c9e0aa2b1260ae0
AniketPant02 <pant.aniket@gmail.com>
2017-08-26 11:07:37 -0500

    Added Project Files
    Added Project Files

---
+++ ClinicalResearch/app/src/main/java/com/clinicalresearch/clinicalresearch/screen_splash.java
@@ -0,0 +1,13 @@
+package com.clinicalresearch.clinicalresearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class screen_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_splash);
+    }
+}
